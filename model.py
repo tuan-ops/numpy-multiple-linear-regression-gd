c95c714d8d547d47c0e06f0bc056a94b702f6765
Nguyễn Anh Tuấn <nguyenanhtuan2111nd@gmail.com>
2026-09-15 00:01:22 +0700

    Step 2: split_train_val_test

--- model.py
+++ model.py
@@ -40,8 +40,25 @@ def shuffle_xy(X, y, seed=42):
     y_shuffled = y[indices]
     return X_shuffled, y_shuffled
 
-# Step 2 - split_train_val_test (not yet solved)
-# TODO: implement
+# Step 2 - split_train_val_test
+def split_train_val_test(X, y, train_frac=0.6, val_frac=0.2):
+    """
+        Bước này để chia dữ liệu thành các tệp train/ validation/ test
+        sau khi dữ liệu đã trộn 
+        Đầu bài cho tỉ lệ giữa tập train và tập val với dữ liệu ban đầu
+        Yêu cầu trả về 3 tập riêng biệt (X, y)
+    """
+    n = len(X)
+    n_train = int(n * train_frac)
+    n_val = int(n * val_frac)
+    n_test = n - n_train - n_val
+    X_train = X[:n_train, :]
+    y_train = y[:n_train]
+    X_val = X[n_train:n_train + n_val , :]
+    y_val = y[n_train:n_train + n_val ]
+    X_test = X[n_train + n_val:, :]
+    y_test = y[n_train + n_val:]
+    return (X_train, y_train, X_val, y_val, X_test, y_test)
 
 # Step 3 - compute_feature_stats (not yet solved)
 # TODO: implement
